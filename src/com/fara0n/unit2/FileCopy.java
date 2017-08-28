07ac43e838f7f72ad9146d38a7b63298821dc0dd
Fara0nV <link.biz@ukr.net>
2017-08-28 20:51:32 +0300

    File copy

---
+++ src/com/fara0n/unit2/FileCopy.java
@@ -0,0 +1,19 @@
+package com.fara0n.unit2;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream=new FileInputStream(" d : \ data.txt ");
+        FileOutputStream fileOutputStream=new FileOutputStream(" d : \ result.txt");
+        while (fileInputStream.available()>0){
+            int data=fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+
+    }
+}
